1c1953e5e605cef2ab110f96b7d3fc614966b258
rahul <kumawatrahul960@gmail.com>
2023-02-19 00:01:54 +0530

    removing js file

--- index.js
+++
@@ -1,8 +0,0 @@
-const gameBoard = document.getElementById("game-board");
-const cells = gameBoard.getElementsByTagName("td");
-let currentPlayer = "X";
-
-    gameBoard.addEventListener("click", function(event) {
-    console.log(event)
-    
-  });
