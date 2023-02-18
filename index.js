ca6af27297c82aadd1b64796fb9476c484747d69
rahul <kumawatrahul960@gmail.com>
2023-02-18 23:51:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const gameBoard = document.getElementById("game-board");
+const cells = gameBoard.getElementsByTagName("td");
+let currentPlayer = "X";
+
+    gameBoard.addEventListener("click", function(event) {
+    console.log(event)
+    
+  });
